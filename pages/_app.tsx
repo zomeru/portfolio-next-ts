c4914f2f90031e3cd5fb64a25812703bd73e1479
Zomer <zomerzxc.19@gmail.com>
2021-07-25 22:24:55 +0800

    Start working with layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className='grid grid-cols-12 gap-6 px-4 lg:px-48 my-14'>
+      <div className='col-span-12 p-4 text-center bg-white lg:col-span-3 rounded-2xl'>
+        Sidebar
+      </div>
+      <div className='col-span-12 bg-white lg:col-span-9 rounded-2xl'>
+        <Component {...pageProps} />
+      </div>
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
