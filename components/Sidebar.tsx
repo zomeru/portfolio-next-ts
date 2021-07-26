7876ea96bc479773faf259c3320c95288c15c62b
Zomer <zomerzxc.19@gmail.com>
2021-07-26 20:28:07 +0800

    Change name font

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -5,7 +5,6 @@ import {
   AiOutlineInstagram,
 } from 'react-icons/ai';
 import { GoLocation } from 'react-icons/go';
-import { GiTie } from 'react-icons/gi';
 
 interface SidebarProps {}
 
@@ -17,7 +16,7 @@ const Sidebar: React.FC<SidebarProps> = ({}) => {
         alt={'user avatar'}
         className='w-32 h-32 mx-auto rounded-full'
       />
-      <h3 className='my-4 text-3xl font-medium tracking-wider font-kaushan'>
+      <h3 className='my-4 text-3xl font-medium tracking-wider font-opensans'>
         <span className='text-blue'>Zomer</span>
         Gregorio
       </h3>
@@ -29,22 +28,20 @@ const Sidebar: React.FC<SidebarProps> = ({}) => {
         className='flex items-center justify-center px-2 py-1 my-3 bg-gray-200 rounded-full'
         download='name'
       >
-        <GiTie className='w-6 h-6' />
         Download Resume
       </a>
       {/* {Social Icons} */}
       <div className='flex justify-around w-9/12 mx-auto my-5 text-blue md:w-full'>
         <a href=''>
-          <AiFillGithub className='w-8 h-6 cursor-pointer' />
+          <AiFillGithub className='w-8 h-8 cursor-pointer' />
         </a>
         <a href=''>
-          <AiFillLinkedin className='w-8 h-6 cursor-pointer' />
+          <AiFillLinkedin className='w-8 h-8 cursor-pointer' />
         </a>
         <a href=''>
           <AiOutlineInstagram className='w-8 h-8 cursor-pointer' />
         </a>
       </div>
-      {/* {Address} */}
       <div
         className='py-4 my-5 bg-gray-200'
         style={{ marginLeft: '-1rem', marginRight: '-1rem' }}
@@ -56,7 +53,6 @@ const Sidebar: React.FC<SidebarProps> = ({}) => {
         <p className='my-2'>zomergregorio@gmail.com</p>
         <p className='my-2'>09303320282</p>
       </div>
-      {/* Email button */}
       <button
         className='w-8/12 px-5 py-2 my-2 text-white rounded-full bg-gradient-to-r from-blue-400 to-blue-600 focus:outline-none'
         onClick={() => window.open('mailto:zomergregorio@gmail.com')}
