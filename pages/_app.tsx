256010315892f081af9546c06e6092bd811c0a88
Zomer <zomerzxc.19@gmail.com>
2021-07-26 16:53:49 +0800

    Add navbar to app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
+import Sidebar from '../components/Sidebar';
+import Navbar from '../components/Navbar';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div className='grid grid-cols-12 gap-6 px-4 lg:px-48 my-14'>
+    <div className='grid grid-cols-12 gap-6 px-4 lg:px-42 my-14 sm:px-20 md:px-32'>
       <div className='col-span-12 p-4 text-center bg-white lg:col-span-3 rounded-2xl'>
-        Sidebar
+        <Sidebar />
       </div>
-      <div className='col-span-12 bg-white lg:col-span-9 rounded-2xl'>
+      <div className='flex flex-col col-span-12 bg-white lg:col-span-9 rounded-2xl'>
+        <Navbar />
         <Component {...pageProps} />
       </div>
     </div>
